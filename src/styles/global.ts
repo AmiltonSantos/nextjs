fea6295cd57145943004fe48439076166581117a
Amilton Santos <amilton@artnew.com.br>
2021-01-24 18:02:06 -0300

    :bug: Corrige css da imagem de fundo

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -35,7 +35,7 @@ export default createGlobalStyle`
 
   body, html {
     background-color: #000000;
-    backgroundImage: url(../assets/background.svg);
+    background-image: url('../assets/background.svg');
     background-attachment: fixed;
     background-size: cover;
     /* background by SVGBackgrounds.com */
